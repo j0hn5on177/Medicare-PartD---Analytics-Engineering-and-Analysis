9856dc33bfc47a9c5154e5f2bb2655f6e40a5df4
johnson liang <johnsonliang177@gmail.com>
2025-03-04 20:05:32 +0000

    f

--- analyses/highest_cost_drugs.sql
+++ analyses/highest_cost_drugs.sql
@@ -2,7 +2,7 @@ SELECT
     dy.year,
     d.brnd_name, 
     SUM(fp.total_cost) AS total_cost
-FROM {{ ref('fact_prescribers') }} fp
+FROM {{ ref('fact_prescription') }} fp
 JOIN {{ ref('dim_drug') }} d ON fp.drug_id = d.drug_id
 JOIN {{ ref('dim_year') }} dy ON fp.year_id = dy.year_id
 GROUP BY dy.year, d.brnd_name
